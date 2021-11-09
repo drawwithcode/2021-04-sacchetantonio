5f096ef8365ce1646acd771251bb01430d0bd9f7
sacchetantonio <antonio.sacchet@mail.polimi.it>
2021-11-09 21:59:01 +0100

    Squiddy Game

--- main.js
+++ main.js
@@ -8,8 +8,9 @@ let masks = [];
 let maskButton;
 console.log(masks);
 
-//camera
+//record image
 let camRec;
+let n = 1;
 
 //intro
 let intro = "hi, " + username;
@@ -38,7 +39,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background("black");
 
-  //cam record
+  //record image
   camRec = createCapture(VIDEO);
   camRec.size(640 * n, 480 * n);
   camRec.hide();
@@ -52,22 +53,26 @@ function setup() {
   maskButton.mousePressed(changeMask);
   maskButton.class("maskButton");
   mask = random(masks);
+
   push();
   textSize(15);
   text("PRESS HERE TO CHANGE YOUR MASK", 400, windowHeight - 130);
   pop();
+
+  //play song
   track.loop();
 }
 
 function draw() {
-  imageMode(CENTER);
-
-  //logo
+  //logo image
   push();
+  imageMode(CENTER);
   image(logo, windowWidth / 2 + 250, windowHeight - 120, 100, 50);
   pop();
 
-  //camera view
+  imageMode(CENTER);
+
+  //record image
   push();
   translate(windowWidth, 0);
   scale(-1, 1);
@@ -76,10 +81,10 @@ function draw() {
   //   filter(GRAY);
   pop();
 
-  //mask
+  //mask image
   image(mask, windowWidth / 2, windowHeight / 2, 440, 400);
 
-  //text arrows
+  //text change effect
   push();
   textSize(10);
   text("use <-arrows-> to remove the effect", 400, windowHeight - 100);
@@ -92,12 +97,12 @@ function draw() {
   pop();
 }
 
-//function to change mask
+// function to have random mask
 function changeMask() {
   mask = random(masks);
 }
 
-//typing intro
+//function to type on the screen
 function introWriter(sentence, n, x, y, speed) {
   if (n < sentence.length) {
     text(sentence.substring(0, n + 1), x, y);
@@ -106,14 +111,13 @@ function introWriter(sentence, n, x, y, speed) {
     textStyle(BOLD);
     textSize(50);
     fill("white");
-
     setTimeout(function () {
       introWriter(sentence, n, x, y, speed);
     }, speed);
   }
 }
 
-//function press S
+//function to press S
 function keyTyped() {
   if (key === "s" || key === "S") save(username + "'s mask.jpg");
 }
@@ -126,7 +130,7 @@ function keyTyped() {
 //   }
 // }
 
-//function press ARROWS
+//function to use ARROWS
 function keyPressed() {
   if (keyCode === LEFT_ARROW) {
     redness = 150;
